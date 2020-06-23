1e930f3837b55525dfbf4b306903a92b96c9b1e9
Mandel <mandel.vaubourg@viacesi.fr>
2020-06-23 18:24:15 +0200

    conf

--- backend/trustfactor/src/main/java/fr/exiaaix/backend/trustfactor/TrustFactorServiceBean.java
+++ backend/trustfactor/src/main/java/fr/exiaaix/backend/trustfactor/TrustFactorServiceBean.java
@@ -4,10 +4,7 @@ import javax.ejb.Stateless;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- * Configures JAX-RS for the application.
- * @author Juneau
- */
+
 @Stateless
 public class TrustFactorServiceBean {
     
