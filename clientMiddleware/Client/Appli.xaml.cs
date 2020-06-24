9acdf71547a0aa2caed7739038d6a5776511a21e
Deunis <denis.fevre@protonmail.com>
2020-06-24 16:07:41 +0200

    Add login and main page

---
+++ clientMiddleware/Client/Appli.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Client
+{
+    /// <summary>
+    /// Logique d'interaction pour appli.xaml
+    /// </summary>
+    public partial class Appli : Window
+    {
+        public Appli()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFiles_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    fileUpload.Items.Add(System.IO.Path.GetFileName(filename));
+            }
+        }
+    }
+}
--- clientMiddleware/Client/MainWindow.xaml.cs
+++ clientMiddleware/Client/MainWindow.xaml.cs
@@ -1,17 +1,27 @@
-﻿using System;
+using System;
 using Middleware;
 using System.ServiceModel;
+using System.Text;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace Client
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private InstanceContext instanceContext;
         private EndpointClient endpointClient;
+        Appli appli = new Appli();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -26,6 +36,38 @@ namespace Client
         public void Notify(Message message)
         {
             Console.WriteLine(message.OperationName);
+        }
+
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Mail vide.";
+                textBoxEmail.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Mail invalide.";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            else
+            {
+                string email = textBoxEmail.Text;
+                string password = passwordBox1.Password;
+
+                if (passwordBox1.Password.Length == 0)
+                {
+                    errormessage.Text = "Mot de Passe vide.";
+                    passwordBox1.Focus();
+                }
+                else
+                {
+                    appli.Show();
+                    Close();
+                }
+            }
         }
     }
 
